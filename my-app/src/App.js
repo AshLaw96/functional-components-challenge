f92bf5d401c62cbc813790f5b73359144da0fb6f
AshLaw96 <ash.l.96@outlook.com>
2025-03-11 11:57:49 +0000

    Add code on how to use the component will unmount method

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import css from "./App.module.css";
-import LifeCycleCDU from "./components/LifeCycleCDU";
+import LifeCyclesCWU from "./components/LifeCyclesCWU";
 
 function App() {
   return (
     <div className={css.App}>
-      <LifeCycleCDU />
+      <LifeCyclesCWU />
     </div>
   );
 }
